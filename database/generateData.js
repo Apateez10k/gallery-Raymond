cc6f3a0f25a35359f3353cf49bfc26a212f7b5e7
Raymond Cabrera <rjgcengr@gmail.com>
2018-05-05 12:32:03 -0700

    add file format and record quantity cmd line arguments to data generation script

--- database/generateData.js
+++ database/generateData.js
@@ -1,24 +1,70 @@
+// if there are no required arguments, exit
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log('Missing filename or quantity arguments');
+  console.log('Correct use: node generateData.js <filename ending in .tsv, .csv, or .json> <number of records to generate>');
+  process.exit(1);
+}
+
+// if the filename is incorrect, exit
+if (!process.argv[2].includes('.tsv') && !process.argv[2].includes('.csv') && !process.argv[2].includes('.json')) {
+  console.log('Filename argument is not .tsv, .csv, or .json format');
+  process.exit(1);
+}
 
 const fs = require('fs');
 const faker = require('faker');
 
+// file format flags
+var isTSV = process.argv[2].includes('.tsv');
+var isCSV = process.argv[2].includes('.csv');
+var isJSON = process.argv[2].includes('.json');
+
+var numRecords = parseInt(process.argv[3]);
+
+
 var options = {
   autoClose: true
 };
-var writeStream = fs.createWriteStream('seedData.tsv', options);
+var writeStream = fs.createWriteStream(process.argv[2], options);
+
+
+var photoArr;
+if (isJSON) {
+  photoArr = `["https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco","https://loremflickr.com/3000/2000/sanfrancisco"]`;
+} else {
+  photoArr = "{https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco}";
+}
+
+var delimiter;
+if (isTSV) {
+  delimiter = '\t';
+} else if (isCSV) {
+  delimiter = ',';
+}
+
 var i = 1;
-var photoArr = "{https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco,https://loremflickr.com/3000/2000/sanfrancisco}";
 var write = function() {
   var ok = true;
-  while (i <= 10000000 && ok) {
-    if (i === 10000000) {
-      writeStream.write(`${i}\t${faker.company.companyName()}\t"${photoArr}"\n`);
+  while (i <= numRecords && ok) {
+    if (!isJSON) {
+      if (i === numRecords) {
+        writeStream.write(`${i}${delimiter}"${faker.company.companyName()}"${delimiter}"${photoArr}"\n`);
+      } else {
+        ok = writeStream.write(`${i}${delimiter}"${faker.company.companyName()}"${delimiter}"${photoArr}"\n`);
+      }
     } else {
-      ok = writeStream.write(`${i}\t${faker.company.companyName()}\t"${photoArr}"\n`);
+      if (i === 1) {
+        writeStream.write(`[{"id":${i},"name":"${faker.company.companyName()}","photos":${photoArr}}\n`);
+      } else if (i === numRecords) {
+        writeStream.write(`{"id":${i},"name":"${faker.company.companyName()}","photos":${photoArr}}]`);
+      } else {
+        ok = writeStream.write(`{"id":${i},"name":"${faker.company.companyName()}","photos":${photoArr}},\n`);
+      }
+
     }
     i++;
   }
-  if (i <= 10000000) {
+  if (i <= numRecords) {
     writeStream.once('drain', write);
   }
 };
